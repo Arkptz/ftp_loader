6d0f1f69ebfb3b6264d883b88d382510545c4948
Arkptz <arkptz@gmail.com>
2024-01-20 23:03:35 +0300

    add "multithread"

--- ftp_loader/main.py
+++ ftp_loader/main.py
@@ -7,32 +7,41 @@ import aiohttp
 from ftp_loader.config import SETTINGS
 
 
-async def launch():
+async def launch(full_path):
+    print(f"adding {full_path}....")
+    data = aiohttp.FormData()
+    # Используем aiofiles для асинхронного чтения файла
+    async with aiofiles.open(full_path, "rb") as file:
+        file_content = await file.read()  # Асинхронное чтение содержимого файла
+        data.add_field(
+            "file",
+            file_content,
+            filename="random",
+            content_type="application/octet-stream",
+        )
+
+    url = "https://cdn.vitrinagram.ru/api/ftp/files/load_file"
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, data=data) as resp:
+            print(await resp.json())
+            if resp.status == 200:
+                print(f"Success added {full_path}....")
+
+
+async def start():
+    files_to_add = []
     files = os.listdir(SETTINGS.path_with_images)
     for file_name in files:
         full_path = os.path.join(SETTINGS.path_with_images, file_name)
         if os.path.isfile(full_path):
-            print(file_name)
-            data = aiohttp.FormData()
-            # Используем aiofiles для асинхронного чтения файла
-            async with aiofiles.open(full_path, "rb") as file:
-                file_content = await file.read()  # Асинхронное чтение содержимого файла
-                data.add_field(
-                    "file",
-                    file_content,
-                    filename=file_name,
-                    content_type="application/octet-stream",
-                )
-
-            url = "https://cdn.vitrinagram.ru/api/ftp/files/load_file"
-            async with aiohttp.ClientSession() as session:
-                async with session.post(url, data=data) as resp:
-                    print(await resp.json())
+            files_to_add.append(full_path)
+    tasks = [asyncio.create_task(launch(full_path=path)) for path in files_to_add]
+    await asyncio.wait(tasks)
 
 
 def main():
-    asyncio.run(launch())
+    asyncio.run(start())
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
